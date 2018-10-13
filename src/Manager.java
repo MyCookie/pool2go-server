545f914e09a109a94b023f44f6d66868394c77c4
Ashish Kansara <ashishnkansara@gmail.com>
2018-10-12 22:26:29 -0600

    Build simple Manager with logging capabilities.

--- src/Manager.java
+++ src/Manager.java
@@ -1,9 +1,35 @@
+import java.io.IOException;
+import java.util.logging.*;
+
 /**
  * Main
  */
 public class Manager {
 
     public static void main(String [] argv) {
-        System.out.println("Server says hi! :)");
+
+        // build logger
+        Logger logger = Logger.getLogger((new Manager()).getClass().getSimpleName()); // change to another name?
+        Handler consoleHandler;
+        Handler fileHandler;
+
+        try {
+            consoleHandler = new ConsoleHandler();
+            fileHandler = new FileHandler("logger." + Manager.class.getSimpleName() + ".log");
+            fileHandler.setFormatter(new SimpleFormatter());
+
+            logger.addHandler(consoleHandler);
+            logger.addHandler(fileHandler);
+
+            consoleHandler.setLevel(Level.ALL);
+            fileHandler.setLevel(Level.ALL);
+            logger.setLevel(Level.ALL);
+
+            logger.config("Logger configuration finished.");
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "IOException thrown building log.", e);
+        }
+
+        logger.log(Level.INFO, "Manager says hi! :)");
     }
 }
