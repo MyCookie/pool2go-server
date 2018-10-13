7648ffb831f176890c6ad85fc8afb42f075378d0
Ashish Kansara <ashishnkansara@gmail.com>
2018-10-12 22:28:31 -0600

    Created simple server with logging capabilities.

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.*;
 
-public class Server {
+public class Server implements Runnable {
+
+    private ServerSocket listener;
+    private Logger logger;
+
+    public Server(int port) throws IOException {
+        Handler fileHandler = new FileHandler("logger." + this.getClass().getSimpleName() + ".log");
+        fileHandler.setFormatter(new SimpleFormatter());
+
+        logger = Logger.getLogger(this.getClass().getSimpleName());
+        logger.addHandler(new ConsoleHandler());
+        logger.addHandler(fileHandler);
+
+        fileHandler.setLevel(Level.ALL);
+        logger.setLevel(Level.ALL);
+
+        logger.log(Level.CONFIG, "Server logger configured");
+
+        listener = new ServerSocket(port);
+        logger.log(Level.CONFIG, "Server listener created");
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                Socket socket = listener.accept();
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String s = in.readLine();
+
+                logger.log(Level.INFO, "New input received");
+
+                if (Thread.interrupted()) {
+                    logger.log(Level.WARNING, "Server interrupted in loop.");
+                    return;
+                }
+            }
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
 }
