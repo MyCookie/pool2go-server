f2c03a1398890f0949ea992fee32bc0890e85daf
Ashish Kansara <ashishnkansara@gmail.com>
2018-10-18 17:07:47 -0600

    Create a new Locations table in DB when starting a new Server.
    Also a small fix: made sure to to close the connection to the DB when
    interrupting the server.

--- src/Server.java
+++ src/Server.java
@@ -2,10 +2,7 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.Connection;
-import java.sql.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.logging.*;
 
 public class Server implements Runnable {
@@ -29,17 +26,38 @@ public class Server implements Runnable {
             throw new IOException("Could not build Server Logger.");
         }
 
+        dbUrl = "jdbc:sqlite:" + databaseUrl;
+
         try {
-            dbUrl = "jdbc:sqlite:" + databaseUrl;
-            connection = DriverManager.getConnection(dbUrl);
-            logger.log(Level.CONFIG, "Registered database at: " + databaseUrl);
+            buildDb();
         } catch (SQLException e) {
-            logger.log(Level.SEVERE, "Could not create connection to given database.");
             throw new IOException("Could not create database.");
         }
 
         listener = new ServerSocket(port);
         logger.log(Level.CONFIG, "Server listener created on port: " + port);
+    }
+
+    private void buildDb() throws SQLException {
+        try {
+            connection = DriverManager.getConnection(dbUrl);
+            logger.log(Level.CONFIG, "Registered database at: " + dbUrl);
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "Could not create connection to given database.");
+            throw new SQLException(e);
+        }
+
+        try {
+            String sqlCreateTable = "CREATE TABLE IF NOT EXISTS Locations (\n" +
+                    " key text PRIMARY KEY,\n" +
+                    " latitude real,\n" +
+                    " longitude real\n" +
+                    ");";
+            connection.createStatement().execute(sqlCreateTable);
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "Could not create Locations table in database.");
+            throw new SQLException(e);
+        }
     }
 
     /**
@@ -87,11 +105,14 @@ public class Server implements Runnable {
 
                 if (Thread.interrupted()) {
                     logger.log(Level.WARNING, "Server interrupted in loop.");
+                    connection.close();
                     return;
                 }
             }
         } catch (IOException e) {
             e.getMessage();
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, "Could not close connection to SQLite DB.");
         }
     }
 }
