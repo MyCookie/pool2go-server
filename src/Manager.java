7cb47c7e45de3e34a411fde5101dcab2776134b0
Ashish Kansara <ashishnkansara@gmail.com>
2018-10-17 19:14:50 -0600

    Manager reads command line arguments to configure Server.
    There are no longer any defaults when starting the server, all arguments
    must be passed from the command line.
    
    The path must include a forward-slash at the end. TODO: check for the
    forward slash when reading the path.
    
    The file name must include a file extension if needed.
    
    TODO: check if the port is in a valid range before continuing.

--- src/Manager.java
+++ src/Manager.java
@@ -15,16 +15,65 @@ public class Manager {
     private Connection connection;
     private Thread server;
 
-    // TODO: replace static objects with reading arguments
-    public static final int ARG_DB_PATH = 0;
-    public static final int ARG_PORT = 1;
+    private static final int ARG_DB_PATH = 0;
+    private static final int ARG_DB_NAME = 2;
+    private static final int ARG_PORT = 4;
 
     public static void main(String [] argv) {
 
+        /* remove comment block to check argv contents unabridged
+        String s = "";
+        for (String i : argv)
+            s += i + " ";
+        System.out.println(s);
+        */
+
+        String path = null;
+        String filename = null;
+        int port = 0;
+
+        // parse arguments
+        boolean valid_args = true;
+
+        if (argv.length > 0) {
+            if (argv[ARG_DB_PATH].equals("--help") || argv[ARG_DB_PATH].equals("-h")) valid_args = false;
+
+            if (argv[ARG_DB_PATH].equals("--path") || argv[ARG_DB_PATH].equals("-a"))
+                path = argv[ARG_DB_PATH + 1];
+            else
+                valid_args = false;
+
+            if (argv[ARG_DB_NAME].equals("--filename") || argv[ARG_DB_NAME].equals("-f"))
+                filename = argv[ARG_DB_NAME + 1];
+            else
+                valid_args = false;
+
+            if (argv[ARG_PORT].equals("--port") || argv[ARG_PORT].equals("-p"))
+                port = Integer.parseInt(argv[ARG_PORT + 1]);
+            else
+                valid_args = false;
+        } else
+            valid_args = false;
+
+        if (!valid_args) {
+            System.out.println("Invalid arguments.\n" +
+                               "Valid arguments:" + "\n" +
+                               "    --path, -a                full path for the SQLite database" + "\n" +
+                               "    --filename, -f            full name (including extension) for the SQLite database" + "\n" +
+                               "    --port, -p                port number for the server");
+            return;
+        } else {
+            System.out.println("Running with the following arguments:" + "\n" +
+                               "Full path for SQLite DB: " + path + "\n" +
+                               "File name for SQLite DB: " + filename + "\n" +
+                               "Server running on port:  " + Integer.toString(port) + "\n");
+        }
+
+        // Start the manager
         Manager manager = null;
 
         try {
-            manager = new Manager(argv);
+            manager = new Manager(path, filename, port);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Could not instantiate Manager.");
@@ -44,9 +93,10 @@ public class Manager {
         manager.closeDb();
     }
 
-    public Manager(String [] args) throws Exception {
+    public Manager(String path, String filename, int port) throws Exception {
         // build logger
         logger = Logger.getLogger(this.getClass().getSimpleName()); // change to another name?
+        logger.log(Level.CONFIG, "Current working directory: " + new File(".").getCanonicalPath());
 
         try {
             loggerFactory();
@@ -59,10 +109,7 @@ public class Manager {
         connection = null;
 
         try {
-            dbFactory(args);
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Could not build database.");
-            return;
+            dbFactory(path, filename);
         } catch (SQLException e) {
             logger.log(Level.SEVERE, "Could not create connection to SQL database.");
             throw new Exception("Could not create DB.");
@@ -72,7 +119,7 @@ public class Manager {
         server = null;
 
         try {
-            server = new Thread(new Server(8080));
+            server = new Thread(new Server(port, path + filename));
         } catch (IOException e) {
             logger.log(Level.SEVERE, "IOException thrown starting Server.", e);
             throw new Exception("Could not start server.");
@@ -99,27 +146,15 @@ public class Manager {
         logger.config("Logger configuration finished.");
     }
 
-    private void dbFactory(String [] args) throws IOException, SQLException {
-        String dbFilePath = null;
-        String dbUrl = "jdbc:sqlite:";
+    private void dbFactory(String path, String filename) throws SQLException {
+        String dbUrl = "jdbc:sqlite:" + path + filename;
 
-        logger.log(Level.CONFIG, "Current working directory: " + new File(".").getCanonicalPath());
-
-        if (args.length > 0) {
-            dbFilePath = args[ARG_DB_PATH];
-            logger.log(Level.CONFIG, "Using custom database file path: " + dbFilePath);
-        } else {
-            try {
-                dbFilePath = new File(".").getCanonicalPath();
-                logger.log(Level.CONFIG, "Using current working directory as database file path: " + dbFilePath);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        dbUrl += dbFilePath + "pool2go.db";
+        logger.log(Level.CONFIG, "Using custom database file path: " + path);
+        logger.log(Level.CONFIG, "Using custom file name for DB: " + filename);
+        logger.log(Level.CONFIG, "Full URL for SQLite-JDBC driver: " + dbUrl);
 
         connection = DriverManager.getConnection(dbUrl);
+        logger.log(Level.CONFIG, "Successfully connected to SQLite DB.");
     }
 
     public void stopServer() {
