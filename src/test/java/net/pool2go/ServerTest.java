c46825b21f4cfae439258c16cb9b2e4967e12683
Ashish Kansara <ashishnkansara@gmail.com>
2018-10-30 18:48:39 -0600

    Add new test to simulate two different clients within range.
    To simulate different clients from the same IP, the client waits a
    couple of seconds to create a new connection. It also does not remember
    it's key sent by the server.

--- src/test/java/net/pool2go/ServerTest.java
+++ src/test/java/net/pool2go/ServerTest.java
@@ -5,11 +5,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.sql.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -29,6 +28,15 @@ class ServerTest {
         } catch (IOException e) {
             System.out.println("Do not have read/write permissions in " + currentWorkingDirectory);
         }
+
+        // clear the database before running a new batch of tests
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + currentWorkingDirectory + databaseFileName);
+            String sqlDropTable = "DROP TABLE IF EXISTS Locations";
+            connection.createStatement().execute(sqlDropTable);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @BeforeEach
@@ -36,7 +44,7 @@ class ServerTest {
         try {
             server = new Thread(new Server(TEST_PORT, currentWorkingDirectory + databaseFileName));
         } catch (IOException e) {
-            System.out.println(e.getCause());
+            System.out.println(e.getMessage());
         }
 
         server.start();
@@ -52,7 +60,7 @@ class ServerTest {
 
         try {
             client = new Socket("localhost", TEST_PORT);
-            in = new ObjectInputStream(client.getInputStream());
+            in = new ObjectInputStream(client.getInputStream()); // start handshake
             locationObject = (LocationObject) in.readObject();
             out = new ObjectOutputStream(client.getOutputStream());
             out.writeObject(locationObject);
@@ -67,12 +75,122 @@ class ServerTest {
             server.interrupt();
             client.close();
         } catch (ClassNotFoundException e) {
-            System.out.println(e.getCause());
+            System.out.println(e.getMessage());
         } catch (IOException e) {
-            System.out.println(e.getCause());
+            System.out.println(e.getMessage());
         }
 
         assertTrue(locationObject.getKey().contains(ip));
+    }
+
+    /**
+     * To test "different" IPs, we'll exploit how keys are generated on the server with no knowledge of what or where
+     * the client is.
+     */
+    @Test
+    void simulateTwoClientsWithinBounds() {
+        Socket client = null;
+        ObjectInputStream in = null;
+        ObjectOutputStream out = null;
+        LocationObject locationObject = null;
+        LocationObject clientOneLocationObject = new LocationObject(5.001, 5.001);
+        LocationObject clientTwoLocationObject = new LocationObject(5.003, 5.003);
+        String clientOneIp = null;
+        String clientTwoIp = null;
+        String clientOneKey = null;
+        String clientTwoKey = null;
+
+        // client one communicates to server
+        try {
+            client = new Socket("localhost", TEST_PORT);
+            in = new ObjectInputStream(client.getInputStream());
+            locationObject = (LocationObject) in.readObject();
+            clientOneKey = locationObject.getKey(); // store client key
+            out = new ObjectOutputStream(client.getOutputStream());
+            out.writeObject(locationObject);
+            out.flush();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (byte b : client.getInetAddress().getAddress()) {
+                if (stringBuilder.length() > 0) stringBuilder.append(".");
+                stringBuilder.append(b);
+            }
+            clientOneIp = stringBuilder.toString();
+
+            // send client's location
+            clientOneLocationObject.setKey(clientTwoKey);
+            out.writeObject(clientOneLocationObject);
+            out.flush();
+
+            // server interacts with the database
+
+            // receive the location from the server
+            locationObject = (LocationObject) in.readObject();
+
+            // client has finished communicating with the server
+            client.close();
+        } catch (UnknownHostException e) {
+            System.out.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            // there should be no records in the database, so server sends the out-of-bounds location
+            assertTrue(clientOneKey.contains(clientOneIp));
+            assertTrue(locationObject.getLatitude() == 360 && locationObject.getLongitude() == 360);
+        }
+
+        // wait for the server to generate a new timestamp-based key
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // client two communicates with server
+        try {
+            client = new Socket("localhost", TEST_PORT);
+            in = new ObjectInputStream(client.getInputStream());
+            locationObject = (LocationObject) in.readObject();
+            clientTwoKey = locationObject.getKey(); // store client key
+            out = new ObjectOutputStream(client.getOutputStream());
+            out.writeObject(locationObject);
+            out.flush();
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (byte b : client.getInetAddress().getAddress()) {
+                if (stringBuilder.length() > 0) stringBuilder.append(".");
+                stringBuilder.append(b);
+            }
+            clientTwoIp = stringBuilder.toString();
+
+            // send client's location
+            clientTwoLocationObject.setKey(clientTwoKey);
+            out.writeObject(clientTwoLocationObject);
+            out.flush();
+
+            // server interacts with the database
+
+            // receive the location from the server
+            locationObject = (LocationObject) in.readObject();
+
+            // client has finished communicating with the server
+            client.close();
+        } catch (UnknownHostException e) {
+            System.out.println(e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            // don't need the server anymore
+            server.interrupt();
+
+            assertTrue(clientTwoKey.contains(clientTwoIp));
+            assertEquals(clientOneLocationObject.getLatitude(), locationObject.getLatitude());
+            assertEquals(clientOneLocationObject.getLongitude(), locationObject.getLongitude());
+        }
     }
 
     @AfterEach
